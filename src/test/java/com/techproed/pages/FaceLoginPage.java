27377a8cd386ae94ac1c7fd4fe559c595a5dfdd5
techproed <abayram.techproed@gmail.com>
2020-09-27 15:24:53 -0500

    Webtables, Page Object Model, Configuration Properties

---
+++ src/test/java/com/techproed/pages/FaceLoginPage.java
@@ -0,0 +1,35 @@
+package com.techproed.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+//PAGE CLASS
+//THIS CLASS WILL ONLY HAVE PAGE OBJECTS/PAGE ELEMENTS
+public class FaceLoginPage {
+
+    WebDriver driver;
+
+    //Create the constructor to initialize the driver
+    public FaceLoginPage(WebDriver driver){
+        this.driver=driver;
+        //initElements initializes the driver in page object model
+        PageFactory.initElements(driver,this);
+        //We use initElements to initialize the driver in the constructor
+    }
+
+    @FindBy(id="email")
+    public WebElement username;
+
+    @FindBy(id="pass")
+    public WebElement password;
+
+    @FindBy(xpath="//button[@id='u_0_b']")
+    public WebElement login;
+
+    @FindBy(xpath = "//div[@class='_9ay7']")
+    public WebElement errorMessage;
+
+
+}
---
+++ src/test/java/com/techproed/tests/FaceLogin.java
@@ -0,0 +1,31 @@
+package com.techproed.tests;
+
+import com.techproed.pages.FaceLoginPage;
+import com.techproed.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FaceLogin extends TestBase {
+
+    @Test
+    public void logInTest() throws InterruptedException {
+
+        //Go to the webpage
+        driver.get("https://www.facebook.com/");
+        //Create FaceLoginPage object
+        FaceLoginPage faceLoginPage=new FaceLoginPage(driver);
+        //send username
+        faceLoginPage.username.sendKeys("sdgagarga3erghae");
+        faceLoginPage.password.sendKeys("asdgsadfhgsdhfsd");
+        faceLoginPage.login.click();
+        Thread.sleep(6000);
+
+        String actualErrorMessage=faceLoginPage.errorMessage.getText();
+        String expectedErrorMessage="The email or phone number you’ve entered doesn’t match any account. Sign up for an account.";
+        Assert.assertEquals(actualErrorMessage,expectedErrorMessage);
+
+    }
+
+}
