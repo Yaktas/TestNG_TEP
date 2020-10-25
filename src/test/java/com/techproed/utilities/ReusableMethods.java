6a0dc2d9b007bba62a6e7377655ca903f5a4bd85
techproed <abayram.techproed@gmail.com>
2020-10-25 13:44:21 -0500

    committing extend reports

--- src/test/java/com/techproed/utilities/ReusableMethods.java
+++ src/test/java/com/techproed/utilities/ReusableMethods.java
@@ -28,7 +28,7 @@ public class ReusableMethods {
         File source = ts.getScreenshotAs(OutputType.FILE);
         // full path to the screenshot location
         String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
-
+        //String target = System.getProperty("user.dir") + "\\test-output\\Screenshots\\" + name + date + ".png";
         File finalDestination = new File(target);
         // save the screenshot to the path given
         FileUtils.copyFile(source, finalDestination);
@@ -143,4 +143,6 @@ public class ReusableMethods {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeout);
         return wait.until(ExpectedConditions.textToBe(locator,text));
     }
+
+
 }
--- src/test/java/com/techproed/utilities/TestBase.java
+++ src/test/java/com/techproed/utilities/TestBase.java
@@ -15,21 +15,23 @@ import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
     protected WebDriver driver;
-    protected static ExtentReports extentReports;
-    protected static ExtentTest extentTest;
-    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentReports extentReports;//This is to initialize the extent report
+    protected static ExtentTest extentTest;//This one is to log if a test fails or passes. We use this to attached the screenshot
+    protected static ExtentHtmlReporter extentHtmlReporter;//this is to generate html report.
     @BeforeTest(alwaysRun = true)
     public void setUpTest() {//This is how to set up Extent report. We will create and use this one in out test classes
         extentReports = new ExtentReports();//1. create object to set the location of the report
         String filePath = System.getProperty("user.dir") + "/test-output/myprojectreport.html";//create a custom report in the current project.
-        //Folder name = test-output, File name = report.html
+        //Folder name = test-output, File name = myprojectreport.html
         //String filePath = System.getProperty("user.dir") + "\\test-output\\report.html";//THIS IS FOR WINDOWS USER
         extentHtmlReporter = new ExtentHtmlReporter(filePath);//2. creating the report with the path we created
         extentReports.attachReporter(extentHtmlReporter);//3. attaching the html report to our custom report
         //WE CAN ADD CUSTOM INFO. NOT NECESSARY. JUST TO GIVE MORE INFORMATION TO THE USER OR TEAM
+        //BELOW LINES ARE OPTIONAL> THEY ARE JUST TO ADD ADDITIONAL INFORMATION TO OUR HTML REPORT
+        extentReports.setSystemInfo("Team Name","Jaguars");
         extentReports.setSystemInfo("Environment", "QA");
         extentReports.setSystemInfo("Browser", ConfigReader.getProperty("browser"));
-        extentReports.setSystemInfo("Automation Engineer", "ENGINEER INFORMATION");
+        extentReports.setSystemInfo("Automation Engineer", "John");
         extentHtmlReporter.config().setDocumentTitle("Koala Reports");
         extentHtmlReporter.config().setReportName("Koala Automation Reports");
 
@@ -40,18 +42,16 @@ public abstract class TestBase {
         driver = Driver.getDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(ConfigReader.getProperty("kaola_url"));
+//        driver.get(ConfigReader.getProperty("kaola_url"));
     }
 
     @AfterMethod(alwaysRun = true)//In AfterMethod, we are getting the screenshots and attaching the report when test fails
     public void tearDownMethod(ITestResult result) throws IOException {
-
         if (result.getStatus() == ITestResult.FAILURE) {//When test case fails, then take the screenshot and attached the report
             String screenshotLocation = ReusableMethods.getScreenshot(result.getName());//getScreenshot is coming from ReusableMethods
             extentTest.fail(result.getName());
             extentTest.addScreenCaptureFromPath(screenshotLocation);//adding the screenshot to the report
             extentTest.fail(result.getThrowable());
-
         } else if (result.getStatus() == ITestResult.SKIP) {
             extentTest.skip("Test Case is skipped: " + result.getName());
         }
@@ -61,6 +61,7 @@ public abstract class TestBase {
 
     @AfterTest(alwaysRun = true)
     public void tearDownTest() {
+        //flush will close the extend reports
         extentReports.flush();
     }
 }
